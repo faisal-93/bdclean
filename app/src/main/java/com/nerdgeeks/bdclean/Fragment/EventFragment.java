1c1f68376d87ac2c59908ba7030375de6a3c5b0a
SazzadKanon <sazzadkanon@gmail.com>
2018-01-28 12:18:17 +0600

    lol

--- app/src/main/java/com/nerdgeeks/bdclean/Fragment/EventFragment.java
+++ app/src/main/java/com/nerdgeeks/bdclean/Fragment/EventFragment.java
@@ -2,6 +2,7 @@ package com.nerdgeeks.bdclean.Fragment;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,10 +22,29 @@ public class EventFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_event, container, false);
-    }
+
+
+//        View inflatedView = inflater.inflate(R.layout.fragment_event, container, false);
+//
+//        TabLayout tabEvent = (TabLayout) inflatedView.findViewById(R.id.event_tab);
+//        tabEvent.addTab(tabEvent.newTab().setText("I Joined"));
+//        tabEvent.addTab(tabEvent.newTab().setText("Upcoming"));
+
+//        tabEvent.addOnTabSelectedListener(new tabEvent.OnTabSelectedListener(){
+//            @Override
+//            public void onTabSelected(tabEvent.Tab tab){
+//                if (tabEvent.getSelectedTabPosition() == 0){}
+//                else if (tabEvent.getSelectedTabPosition() == 1){}
+//            }
+//        });
+//
+//        return inflatedView;
+   }
 
 }
+
+
