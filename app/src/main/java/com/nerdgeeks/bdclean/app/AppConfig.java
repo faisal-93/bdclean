5cb935e9d0fabbcfc1b340c1702e2eff96742270
Sunny <faisal.cse16@gmail.com>
2018-02-05 23:01:45 +0600

    server connection

---
+++ app/src/main/java/com/nerdgeeks/bdclean/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.nerdgeeks.bdclean.app;
+
+/**
+ * Created by Sunny on 02/04/2018.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.105:8080/bdclean_api/login.php";
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.105:8080/bdclean_api/register.php";
+}
