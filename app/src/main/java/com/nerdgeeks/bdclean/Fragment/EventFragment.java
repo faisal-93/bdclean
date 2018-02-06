7c4db96e14d5517dbe3cf81e79449750f8e4bea9
SazzadKanon <sazzadkanon@gmail.com>
2018-02-06 20:52:28 +0600

    Kanon

--- app/src/main/java/com/nerdgeeks/bdclean/Fragment/EventFragment.java
+++ app/src/main/java/com/nerdgeeks/bdclean/Fragment/EventFragment.java
@@ -2,9 +2,13 @@ package com.nerdgeeks.bdclean.Fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -25,25 +29,44 @@ public class EventFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        return inflater.inflate(R.layout.fragment_event, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_event, container, false);
 
+        loadFragment(new JoinedFragment());
 
-//        View inflatedView = inflater.inflate(R.layout.fragment_event, container, false);
-//
-//        TabLayout tabEvent = (TabLayout) inflatedView.findViewById(R.id.event_tab);
-//        tabEvent.addTab(tabEvent.newTab().setText("I Joined"));
-//        tabEvent.addTab(tabEvent.newTab().setText("Upcoming"));
+        BottomNavigationView eventNavigation = (BottomNavigationView) rootView.findViewById(R.id.event_bottomNavigation);
+        eventNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        return rootView;
 
-//        tabEvent.addOnTabSelectedListener(new tabEvent.OnTabSelectedListener(){
-//            @Override
-//            public void onTabSelected(tabEvent.Tab tab){
-//                if (tabEvent.getSelectedTabPosition() == 0){}
-//                else if (tabEvent.getSelectedTabPosition() == 1){}
-//            }
-//        });
-//
-//        return inflatedView;
-   }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment eventFragment;
+            switch (item.getItemId()){
+
+                case R.id.joined:
+                    eventFragment = new JoinedFragment();
+                    loadFragment(eventFragment);
+                    return true;
+
+                case R.id.upcoming:
+                    eventFragment = new UpcomingFragment();
+                    loadFragment(eventFragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment eventFragment) {
+        FragmentTransaction eventTransaction = getChildFragmentManager().beginTransaction();
+        eventTransaction.replace(R.id.event_frame_container, eventFragment);
+        eventTransaction.addToBackStack(null);
+        eventTransaction.commit();
+    }
 
 }
 
